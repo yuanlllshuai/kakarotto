22ae7edb6ed1be18a5543000eccdb384e2b3b6e1
yuanlllshuai <yuanlllshuai@gmail.com>
2025-09-03 15:43:16 +0800

    feat: 禁止文字鼠标选中

--- src/main/three/gltf/Name.tsx
+++ src/main/three/gltf/Name.tsx
@@ -10,6 +10,7 @@ const PointLabel = memo(({ begin }: { begin: boolean }) => {
         rotation-y={-Math.PI / 8}
         transform={true}
         className={begin ? style.reflect : style.reflect_hide}
+        style={{ userSelect: "none" }}
       >
         <div>河南省</div>
         <div style={{ fontSize: 13 }}>HENAN PROVINCE</div>
