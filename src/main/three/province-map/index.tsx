93568c39f3166bc26bfc7da4550e8ba4e7ee2074
yuanlllshuai <yuanlllshuai@gmail.com>
2025-11-10 20:52:54 +0800

    feat: 适配各省份大小比例、中心点位置及厚度

--- src/main/three/province-map/index.tsx
+++ src/main/three/province-map/index.tsx
@@ -12,34 +12,74 @@ const { Option } = Select;
 const MapModel = ({ prvince }: { prvince: string }) => {
   const [borders, setBorders] = useState<any[]>([]);
   const [shaps, setShapes] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [scale, setScale] = useState<number>(0);
+
   const parentRef = useRef<any>();
 
   useEffect(() => {
     if (prvince) {
       const loader = new THREE.FileLoader();
+      setLoading(true);
       loader.load(
         `https://geo.datav.aliyun.com/areas_v3/bound/geojson?code=${prvince}_full`,
         function (data) {
           initMap(JSON.parse(data as string));
+          setLoading(false);
         }
       );
     }
   }, [prvince]);
 
   const initMap = (map: any) => {
-    console.log(map);
+    // console.log(map);
     const center = map.features[0].properties.centroid;
-    const projection = d3
+    const projection1 = d3
       .geoMercator()
       .center([center[0], center[1]])
       .scale(80)
       .translate([0, 0]);
     const allBorders: any[] = [];
     const allShaps: any[] = [];
-    map.features.forEach((elem: any, index: number) => {
+    let minX = 0;
+    let maxX = 0;
+    let minZ = 0;
+    let maxZ = 0;
+    let totalX = 0;
+    let totalZ = 0;
+    let total = 0;
+    map.features.forEach((elem: any) => {
       const coordinates = elem.geometry.coordinates;
-      coordinates.forEach((multiPolygon: any, ind: number) => {
+      coordinates.forEach((multiPolygon: any) => {
         multiPolygon.forEach((polygon: any) => {
+          for (let i = 0; i < polygon.length; i++) {
+            const [x, z] = projection1(polygon[i]) as number[];
+            if (!isNaN(x) && !isNaN(z)) {
+              minX = Math.min(minX, x);
+              maxX = Math.max(maxX, x);
+              minZ = Math.min(minZ, z);
+              maxZ = Math.max(maxZ, z);
+              totalX += polygon[i][0];
+              totalZ += polygon[i][1];
+              total += 1;
+            }
+          }
+        });
+      });
+    });
+    const crossX = maxX - minX;
+    const crossZ = maxZ - minZ;
+    setScale(13 / Math.max(crossX, crossZ));
+    const depth = (0.12 * Math.max(crossX, crossZ)) / 2.96;
+    const projection2 = d3
+      .geoMercator()
+      .center([totalX / total, totalZ / total])
+      .scale(80)
+      .translate([0, 0]);
+    map.features.forEach((elem: any, index1: number) => {
+      const coordinates = elem.geometry.coordinates;
+      coordinates.forEach((multiPolygon: any, index2: number) => {
+        multiPolygon.forEach((polygon: any, index3: number) => {
           const lineMaterial = new THREE.LineBasicMaterial({
             color: "white",
           });
@@ -47,17 +87,19 @@ const MapModel = ({ prvince }: { prvince: string }) => {
           const lineGeometry = new THREE.BufferGeometry();
           const positions = new Float32Array(polygon.length * 3);
           for (let i = 0; i < polygon.length; i++) {
-            const [x, z] = projection(polygon[i]) as number[];
-            if (i === 0) {
-              shape.moveTo(x, -z);
+            const [x, z] = projection2(polygon[i]) as number[];
+            if (!isNaN(x) && !isNaN(z)) {
+              if (i === 0) {
+                shape.moveTo(x, -z);
+              }
+              shape.lineTo(x, -z);
+              positions[i * 3] = x;
+              positions[i * 3 + 1] = depth;
+              positions[i * 3 + 2] = z;
             }
-            shape.lineTo(x, -z);
-            positions[i * 3] = x;
-            positions[i * 3 + 1] = 1;
-            positions[i * 3 + 2] = z;
           }
           const extrudeSettings = {
-            depth: 1,
+            depth,
             bevelEnabled: false,
           };
 
@@ -77,9 +119,15 @@ const MapModel = ({ prvince }: { prvince: string }) => {
             new THREE.BufferAttribute(positions, 3)
           );
           const line = new THREE.Line(lineGeometry, lineMaterial);
-          allBorders.push({ line, name: elem.properties.name + index + ind });
+          allBorders.push({
+            line,
+            name: elem.properties.name + index1 + index2 + index3,
+          });
           const mesh = new THREE.Mesh(geometry, [material, material1]);
-          allShaps.push({ mesh, name: elem.properties.name + index + ind });
+          allShaps.push({
+            mesh,
+            name: elem.properties.name + index1 + index2 + index3,
+          });
         });
       });
     });
@@ -87,8 +135,12 @@ const MapModel = ({ prvince }: { prvince: string }) => {
     setShapes(allShaps);
   };
 
+  if (loading) {
+    return <></>;
+  }
+
   return (
-    <object3D ref={parentRef}>
+    <object3D ref={parentRef} scale={scale}>
       {borders.map((i) => (
         <primitive object={i.line} key={i.name} />
       ))}
@@ -134,7 +186,7 @@ function Index() {
             background: new THREE.Color("rgb(2, 3, 34)"),
           }}
         >
-          <axesHelper scale={5} />
+          {/* <axesHelper scale={10} /> */}
           <OrbitControls makeDefault />
           <ambientLight intensity={1} />
           <pointLight position={[10, 10, 10]} decay={0} intensity={3} />
@@ -155,7 +207,13 @@ function Index() {
           <Select
             value={prvince}
             style={{ width: 120 }}
+            showSearch
             onChange={handleChange}
+            filterOption={(input, option: any) =>
+              option.props.children
+                .toLowerCase()
+                .indexOf(input.toLowerCase()) >= 0
+            }
           >
             {prvinces.map(({ name, adcode }) => (
               <Option value={adcode} key={adcode}>
